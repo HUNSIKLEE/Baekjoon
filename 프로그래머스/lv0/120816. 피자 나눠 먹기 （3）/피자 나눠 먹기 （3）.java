af8d1fbdd648262f9d9587f1b80b3ff77d57358c
HUNSIK <108327839+HUNSIKLEE@users.noreply.github.com>
2023-04-01 18:12:31 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,19 @@
+
+public class Solution {
+
+    public int solution(int slice, int n) {
+
+          int answer;
+
+            if (n % slice == 0 ) {
+                return answer = (n / slice);
+
+            } else {
+
+                answer = 1 + (n / slice);
+            }
+
+            return answer;
+        }
+    }
+
