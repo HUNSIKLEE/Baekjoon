ea9a2c90e9650a44fa3d6b1b3122f39d13c4dc94
HUNSIK <108327839+HUNSIKLEE@users.noreply.github.com>
2023-04-04 23:12:30 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+class Solution {
+ public int[] solution ( int n){
+            int[] answer = new int[(n+1) / 2];
+            int b = 0;
+            for (int i = 0; i <= n; i++) {
+                if (i % 2 == 1) {
+                    answer[b] = i;
+                    b++;
+                }
+            }
+
+            return answer;
+        }
+    }
