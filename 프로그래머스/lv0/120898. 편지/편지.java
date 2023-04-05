b78cdb29bd08cf1dcfda2a35d36be500a79bb6ba
HUNSIK <108327839+HUNSIKLEE@users.noreply.github.com>
2023-04-05 16:44:35 +0900

    [level 0] Title: 편지, Time: 0.17 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String message) {
+      int answer = 0;
+
+            String[] arr = message.split("");
+
+                answer = arr.length * 2;
+
+            return answer;
+    }
+}
