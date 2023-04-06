8ecf0e3088ae37cfc4dcbc50921d3adf56bedb89
HUNSIK <108327839+HUNSIKLEE@users.noreply.github.com>
2023-04-06 11:52:21 +0900

    [level 0] Title: 암호 해독, Time: 2.21 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String cipher, int code) {
+       String answer = "";
+
+        String[] arr = cipher.split("");
+
+        for(int i = 1; i <= arr.length; i++){
+            if(i % code == 0) {
+                answer += arr[i - 1];
+            }
+        }
+        return answer;
+    }
+}
