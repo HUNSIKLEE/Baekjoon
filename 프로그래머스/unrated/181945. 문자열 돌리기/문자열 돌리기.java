563599ef40a2b5bd895a809262b067c7d91467b9
HUNSIK <108327839+HUNSIKLEE@users.noreply.github.com>
2023-07-17 14:03:48 +0900

    [unrated] Title: 문자열 돌리기, Time: 137.83 ms, Memory: 66.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String[] b = a.split("");
+
+       for(String s : b){
+           System.out.println(s);
+       }
+
+    }
+}
