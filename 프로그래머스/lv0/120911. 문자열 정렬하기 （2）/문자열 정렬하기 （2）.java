67c5d05bf81f38f71d6da7680ba5c6be3be6b8c1
HUNSIK <108327839+HUNSIKLEE@users.noreply.github.com>
2023-07-22 14:52:34 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 3.12 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+
+
+           String[] a = my_string.toLowerCase().split("");
+           Arrays.sort(a);
+
+           for (int i = 0; i < a.length; i++){
+               answer += a[i];
+           }
+
+
+        return answer;
+    }
+}
