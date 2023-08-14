3a831644e8e6b089c6f6f224de923155d48bd00a
HUNSIK <108327839+HUNSIKLEE@users.noreply.github.com>
2023-08-14 14:16:05 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.93 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] numbers) {
+        return 45 - Arrays.stream( numbers ).reduce( 0, Integer :: sum );
+    }
+}
