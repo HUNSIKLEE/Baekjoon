4cd71ba33cc61a88fec5d1b8947a8e34a75b9146
HUNSIK <108327839+HUNSIKLEE@users.noreply.github.com>
2023-08-19 22:21:32 +0900

    [level 0] Title: 가까운 수, Time: 0.97 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        return Arrays.stream(array)
+                .reduce(array[0], (acc, curr) -> {
+                    int accValue = Math.abs(acc - n);
+                    int currValue = Math.abs(curr - n);
+
+                    if (accValue > currValue || accValue == currValue && acc > curr)
+                        return curr;
+
+                    return acc;
+                });
+    }
+}
